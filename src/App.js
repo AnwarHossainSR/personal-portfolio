91760e1fdb42cb50e077ef1d85d4a2959d736b8a
AnwarHossainSR <mahedianwar@gmail.com>
2022-05-30 22:25:56 +0600

    design Intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import Intro from "./components/Intro/Intro";
 import Navbar from "./components/Navbar/Navbar";
 const App = () => {
+  const darkMode = false;
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
       <Navbar />
+      <Intro />
     </div>
   );
 };
