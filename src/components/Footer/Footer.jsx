5fbcf53aad54c8ff3706b5425467cea8bd53483c
AnwarHossainSR <mahedianwar@gmail.com>
2022-06-01 22:06:13 +0600

    design scroll to top button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,15 +6,32 @@ import Wave from "../../img/wave.png";
 import "./Footer.css";
 
 const Footer = () => {
+  const handleClick = (value) => {
+    window.open(value, "_blank");
+  };
   return (
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
         <span>anwarmahedisr@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <Insta
+            color="white"
+            size={"3rem"}
+            onClick={() =>
+              handleClick("https://www.instagram.com/mahedi_hasan_sr/?hl=en")
+            }
+          />
+          <Facebook
+            color="white"
+            size={"3rem"}
+            onClick={() => handleClick("https://www.facebook.com/MahediSR007")}
+          />
+          <Gitub
+            color="white"
+            size={"3rem"}
+            onClick={() => handleClick("https://github.com/AnwarHossainSR")}
+          />
         </div>
       </div>
     </div>
