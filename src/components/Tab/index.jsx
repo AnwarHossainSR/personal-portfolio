eb3ab6688a4d8f3cde74c80fe606777573a9ceda
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-24 21:37:37 +0600

    added new filter text

---
+++ src/components/Tab/index.jsx
@@ -0,0 +1,6 @@
+
+const Tab = ({ text,className}) => {
+  return <span className={className}>{text}</span>;
+};
+
+export default Tab
--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { FaSearchPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Tab from '../../components/Tab';
 import { projects } from '../../constant/portfolio';
 
 const Portfolio = ({ darkMode }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [])
-  
+  }, []);
+
   return (
     <div className='portfolio-page'>
       <div className='portfolio-page__header'>
@@ -21,9 +21,12 @@ const Portfolio = ({ darkMode }) => {
           Some of my spare time tinkering.
         </p>
         <div className='portfolio-page__header--filter'>
-          <span className='active'>All</span>
-          <span>Web</span>
-          <span>Mobile</span>
+          <Tab className='active' text='All' />
+          <Tab text='React' />
+          <Tab text='Nextjs' />
+          <Tab text='Nodejs' />
+          <Tab text='Laravel' />
+          <Tab text='Native' />
         </div>
       </div>
       <div className='portfolio-page__body'>
