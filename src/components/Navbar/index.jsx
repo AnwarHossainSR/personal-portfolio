a2a3adc12c5d94d8eea14ff88f2b638b83ac0880
Restyled.io <commits@restyled.io>
2022-07-29 11:25:54 +0000

    Restyled by prettier

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,9 @@
-import { motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import { GiCrossMark } from 'react-icons/gi';
-import { GoThreeBars } from 'react-icons/go';
-import { Link, NavLink, useLocation } from 'react-router-dom';
-import Toggle from '../Toggle';
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { GiCrossMark } from "react-icons/gi";
+import { GoThreeBars } from "react-icons/go";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import Toggle from "../Toggle";
 
 const Navbar = ({ darkMode }) => {
   let { pathname } = useLocation();
@@ -11,22 +11,22 @@ const Navbar = ({ darkMode }) => {
 
   useEffect(() => {
     if (open === true && window.innerWidth < 481) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     }
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, [open]);
 
   return (
-    <div className='n-wrapper' id='Navbar'>
+    <div className="n-wrapper" id="Navbar">
       {/* left */}
-      <div className='n-left'>
+      <div className="n-left">
         <NavLink
-          to='/'
-          className='n-name'
+          to="/"
+          className="n-name"
           style={{
-            color: darkMode ? 'white' : '',
+            color: darkMode ? "white" : "",
           }}
         >
           Anwar
@@ -34,84 +34,84 @@ const Navbar = ({ darkMode }) => {
         <Toggle />
       </div>
       {/* right */}
-      <div className='n-right'>
-        <div className='n-list' style={{ display: open && 'block' }}>
-          <ul style={{ listStyleType: 'none' }}>
+      <div className="n-right">
+        <div className="n-list" style={{ display: open && "block" }}>
+          <ul style={{ listStyleType: "none" }}>
             <li>
               <Link
-                style={{ color: darkMode ? 'white' : '' }}
-                to='/'
-                className={`${pathname === '/' && 'active'}`}
+                style={{ color: darkMode ? "white" : "" }}
+                to="/"
+                className={`${pathname === "/" && "active"}`}
               >
                 Home
               </Link>
             </li>
             <li>
               <Link
-                style={{ color: darkMode ? 'white' : '' }}
-                to='/portfolio'
-                className={`${pathname === '/portfolio' && 'active'}`}
+                style={{ color: darkMode ? "white" : "" }}
+                to="/portfolio"
+                className={`${pathname === "/portfolio" && "active"}`}
               >
                 Protfolio
               </Link>
             </li>
             <li>
               <Link
-                style={{ color: darkMode ? 'white' : '' }}
-                to='/about'
-                className={`${pathname === '/about' && 'active'}`}
+                style={{ color: darkMode ? "white" : "" }}
+                to="/about"
+                className={`${pathname === "/about" && "active"}`}
               >
                 About
               </Link>
             </li>
 
-            <div className='navbar-cross'>
+            <div className="navbar-cross">
               <motion.div
                 whileHover={{
                   scale: 1.1,
                   transition: {
                     duration: 0.2,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                   },
                 }}
                 whileTap={{
                   scale: 0.8,
                   transition: {
                     duration: 0.2,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                   },
                 }}
               >
                 <GiCrossMark
                   onClick={() => setOpen(false)}
-                  style={{ fontSize: '2rem', color: 'var(--orange)' }}
+                  style={{ fontSize: "2rem", color: "var(--orange)" }}
                 />
               </motion.div>
             </div>
           </ul>
         </div>
-        <Link to='contact' className='top-contact'>
-          <button className='button n-button'>Contact</button>
+        <Link to="contact" className="top-contact">
+          <button className="button n-button">Contact</button>
         </Link>
-        <div className='navbar-icon'>
+        <div className="navbar-icon">
           <motion.div
             whileHover={{
               scale: 1.1,
               transition: {
                 duration: 0.2,
-                ease: 'easeInOut',
+                ease: "easeInOut",
               },
             }}
             whileTap={{
               scale: 0.8,
               transition: {
                 duration: 0.2,
-                ease: 'easeInOut',
+                ease: "easeInOut",
               },
             }}
           >
             <GoThreeBars
-              style={{ fontSize: '2rem' }}
+              style={{ fontSize: "2rem" }}
               onClick={() => setOpen(true)}
             />
           </motion.div>
