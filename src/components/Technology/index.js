835afd6378e0083e3d5dbadfdbc97050c43fe007
Restyled.io <commits@restyled.io>
2022-07-29 17:58:50 +0000

    Restyled by clang-format

--- src/components/Technology/index.js
+++ src/components/Technology/index.js
@@ -1,14 +1,13 @@
-import { GoTriangleRight } from 'react-icons/go';
+import {GoTriangleRight} from 'react-icons/go';
 
-const Technology = ({ technology }) => {
-  return (
-    <li>
-      <span>
-        <GoTriangleRight style={{ color: 'var(--orange)',fontSize:'.6rem' }} />
+const Technology = ({technology}) => {
+  return (<li><span><GoTriangleRight style =
+                     {
+                       { color: 'var(--orange)', fontSize: '.6rem' }
+                     } />
       </span>
-      <span style={{ marginLeft:5 }}>{technology}</span>
-    </li>
-  );
+          <span style = {{ marginLeft: 5 }}>{technology}</span>
+    </li>);
 };
 
 export default Technology;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,7 @@
 import AnwarImage from '../../assets/img/anwar.svg';
 import Technology from '../../components/Technology';
 
-const About = ({ darkMode }) => {
+const About = ({darkMode}) => {
   return (
     <div className='about'>
       <div className='about-heading'>
@@ -12,11 +12,12 @@ const About = ({ darkMode }) => {
       <div className='about__description'>
         <div className='about__description-left'>
           Hello! My name is Brittany and I enjoy creating things that live on
-          the internet. My interest in web development started back in 2019 when
-          I decided to try editing custom Tumblr themes — turns out hacking
-          together a custom reblog button taught me a lot about HTML & CSS!
-          Fast-forward to today, and I’ve had the privilege of working at an
-          advertising agency, a start-up, a huge corporation, and a student-led
+  the internet.My interest in web development started back in 2019 when
+  I decided to try editing custom Tumblr themes — turns out hacking
+  together a custom reblog button taught me a lot about HTML&CSS!Fast -
+      forward to today,
+      and I’ve had the privilege of working at an
+  advertising agency, a start - up, a huge corporation, and a student - led
           design studio. My main focus these days is building accessible,
           inclusive products and digital experiences at Upstatement for a
           variety of clients. <br />
