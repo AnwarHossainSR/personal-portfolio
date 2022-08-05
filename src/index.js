585c01f9a5aae17206277efe11d99d60f194c71f
Restyled.io <commits@restyled.io>
2022-08-05 06:27:56 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -5,23 +5,17 @@ import './index.css'
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {HelmetProvider} from "react-helmet-async";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
-import { ThemeProvider } from "./context/Context";
+import {ThemeProvider} from "./context/Context";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HelmetProvider>
-    <Provider store={store}>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+root.render(<HelmetProvider><Provider store = {store}><ThemeProvider>
+            <BrowserRouter><App /></BrowserRouter>
       </ThemeProvider>
-    </Provider>
-  </HelmetProvider>
-);
+            </Provider>
+  </HelmetProvider>);
