abb3210fede21e1145820268eba4e877f61bd5d3
Restyled.io <commits@restyled.io>
2022-08-13 11:05:11 +0000

    Restyled by clang-format

--- src/redux/actions/UserAction.js
+++ src/redux/actions/UserAction.js
@@ -1,10 +1,11 @@
-import { addDoc, collection, serverTimestamp } from '@firebase/firestore';
+import {addDoc, collection, serverTimestamp} from '@firebase/firestore';
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
 } from 'firebase/auth';
-import { auth, db } from '../../utils/firebase';
+
+import {auth, db} from '../../utils/firebase';
 import {
   clearUser,
   userFailure,
@@ -17,13 +18,11 @@ export const getAuthUserAction = () => async (dispatch) => {
   try {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        dispatch(
-          userSuccess({
-            accessToken: user.accessToken,
-            email: user.email,
-            id: user.uid,
-          })
-        );
+        dispatch(userSuccess({
+          accessToken : user.accessToken,
+          email : user.email,
+          id : user.uid,
+        }));
       } else {
         dispatch(userFailure());
       }
@@ -36,23 +35,16 @@ export const getAuthUserAction = () => async (dispatch) => {
 export const GetSignInAction = (email, password) => async (dispatch) => {
   dispatch(userPending());
   try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    dispatch(
-      userSuccess({
-        accessToken: userCredential.user.accessToken,
-        email: userCredential.user.email,
-        id: userCredential.user.uid,
-      })
-    );
+    const userCredential =
+        await signInWithEmailAndPassword(auth, email, password);
+    dispatch(userSuccess({
+      accessToken : userCredential.user.accessToken,
+      email : userCredential.user.email,
+      id : userCredential.user.uid,
+    }));
   } catch (error) {
-    console.log(
-      '🚀 ~ file: UserAction.js ~ line 43 ~ GetSignInAction ~ error',
-      error
-    );
+    console.log('🚀 ~ file: UserAction.js ~ line 43 ~ GetSignInAction ~ error',
+                error);
     dispatch(userFailure('Invalid email or password'));
   }
 };
@@ -64,27 +56,26 @@ export const GetLogoutAction = () => async (dispatch) => {
     localStorage.clear();
     dispatch(clearUser());
   } catch (error) {
-    console.log(
-      '🚀 ~ file: UserAction.js ~ line 55 ~ GetLogoutAction ~ error',
-      error
-    );
+    console.log('🚀 ~ file: UserAction.js ~ line 55 ~ GetLogoutAction ~ error',
+                error);
     dispatch(userFailure(error.message));
   }
 };
 
 export const GetCountVisitors = () => async (dispatch) => {
   const count = sessionStorage.getItem('count');
-  if (count) return;
+  if (count)
+    return;
 
   if (navigator.geolocation) {
     console.log('🚀 ~ file: UserAction.js ~ line 69 ~ GetCountVisitors');
     navigator.geolocation.getCurrentPosition(async (position) => {
       sessionStorage.setItem('count', 1);
-      const { latitude, longitude } = position.coords;
+      const {latitude, longitude} = position.coords;
       const visitor = {
         latitude,
         longitude,
-        timestamp: serverTimestamp(),
+        timestamp : serverTimestamp(),
       };
       await addDoc(collection(db, 'visitors'), visitor);
     });
@@ -92,9 +83,9 @@ export const GetCountVisitors = () => async (dispatch) => {
     console.log('Geolocation is not supported by this browser.');
     sessionStorage.setItem('count', 1);
     const visitor = {
-      latitude: 0,
-      longitude: 0,
-      timestamp: serverTimestamp(),
+      latitude : 0,
+      longitude : 0,
+      timestamp : serverTimestamp(),
     };
     await addDoc(collection(db, 'visitors'), visitor);
   }
