85c956aaf70cc8d9eae33d0f25d8557e1509a576
Anwar Hossain <mahedianwar@gmail.com>
2022-11-01 23:21:38 +0600

    design testemonial card and fixed responsivness

--- src/components/app/Testimonials/index.jsx
+++ src/components/app/Testimonials/index.jsx
@@ -1,11 +1,20 @@
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { useEffect, useState } from 'react';
 import { Pagination } from 'swiper';
 import 'swiper/css/pagination';
 import { clients } from '../../../constant/Clients';
 
 const Testimonial = () => {
+  const [mobileScreen, setMobileScreen] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 480) {
+      setMobileScreen(true);
+    }
+  }, []);
+
   return (
     <div className="t-wrapper" id="testimonial">
       <div className="t-heading">
@@ -17,15 +26,17 @@ const Testimonial = () => {
       </div>
       <Swiper
         modules={[Pagination]}
-        slidesPerView={2}
+        slidesPerView={mobileScreen === true ? 1 : 3}
         pagination={{ clickable: true }}
       >
         {clients.map((client, index) => {
           return (
             <SwiperSlide key={index}>
               <div className="testimonial">
+                <p className="review">{client.review}</p>
                 <img src={client.img} alt="" />
-                <span>{client.review}</span>
+                <h3 className="name">{client.name}</h3>
+                <p className="designation">{client.designation}</p>
               </div>
             </SwiperSlide>
           );
