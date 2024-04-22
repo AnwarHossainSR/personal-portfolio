4296b8f1128da6f77c048e8926ee47332c83d39a
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-04-22 16:02:57 +0600

    Update dependencies and add new icons to Footer component

--- iconscout-react-unicons.d.ts
+++ iconscout-react-unicons.d.ts
@@ -1,3 +1,6 @@
 declare module '@iconscout/react-unicons/icons/uil-moon';
 declare module '@iconscout/react-unicons/icons/uil-sun';
+declare module '@iconscout/react-unicons/icons/uil-facebook';
+declare module '@iconscout/react-unicons/icons/uil-github';
+declare module '@iconscout/react-unicons/icons/uil-instagram';
 
