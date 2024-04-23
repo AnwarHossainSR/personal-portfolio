c25c9a03cb3182ad0294da92c727c956f07d8665
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-04-23 16:26:53 +0600

    Update contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactComponent from '@/components/Contact';
+import MainLayout from '@/layouts/MainLayout/MainLayout';
+
+const Contact = () => {
+  return (
+    <MainLayout>
+      <ContactComponent />
+    </MainLayout>
+  );
+};
+
+export default Contact;
