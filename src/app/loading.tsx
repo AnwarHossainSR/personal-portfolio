5c52e70a28d26eaf8d21f393879b1bd2727bf065
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-04-24 15:42:45 +0600

    Add react-loader-spinner package and update loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,47 @@
-const loading = () => {
-  return <div>loading</div>;
+'use client';
+
+import { FallingLines } from 'react-loader-spinner';
+
+import { useTheme } from '@/providers/context/Context';
+
+const Loader = () => {
+  const theme = useTheme();
+  const { darkMode } = theme.state;
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: darkMode ? 'var(--black)' : 'var(--main-bg-light)',
+      }}
+    >
+      <div style={{ width: '100px' }}>
+        <FallingLines
+          color="var(--main-color-orange)"
+          width="100"
+          visible
+          // ariaLabel="falling-circles-loading"
+        />
+        {/* Optional: Loader text */}
+        <p
+          style={{
+            textAlign: 'center',
+            color: 'var(--main-bg-light)',
+            marginTop: '10px',
+          }}
+        >
+          Loading...
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default loading;
+export default Loader;
