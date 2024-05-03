05c32be9fa7e919b028f5bc07c713da0269adfe7
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-05-03 10:38:57 +0600

    chore: Update next.config.js, .env.example, package.json, and Footer component

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['themewagon.github.io'],
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+  maxDuration: 5,
 };
 
 module.exports = nextConfig;
