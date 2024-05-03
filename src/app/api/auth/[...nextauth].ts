df0b9fd54c8d40f8be8e9bba46f5e41a8a08439a
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-05-03 14:37:25 +0600

    Refactor import paths and update PrismaDB reference

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import type { AuthOptions } from 'next-auth';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-import { prisma } from '@/lib/prismaDB';
+import { prisma } from '@/lib';
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
