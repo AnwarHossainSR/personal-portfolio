cd0347cd1606f93e1e6113daeb28822615e5cb66
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-05-09 15:36:05 +0600

    chore: Add uploadToCloudinary function and update exports in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,14 +3,15 @@
 
 // for example if you were using Sanity as your CMS you might want to add:
 // sanity.ts -> methods to interact with Sanity CMS
+import { uploadToCloudinary } from './cloudinary';
 import { getDateCompare } from './date';
 import { createJwtToken, tokenVerify } from './jwtToken';
 import { prisma } from './prismaDB';
 import { displayNumbers } from './utils';
-
 export {
     createJwtToken, displayNumbers,
     getDateCompare,
-    prisma, tokenVerify
+    prisma, tokenVerify,
+    uploadToCloudinary
 };
 
