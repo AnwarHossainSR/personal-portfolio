2bac302abfd1f3d092bf888df5f9682f85d46ecd
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-05-09 16:55:32 +0600

    chore: Update blog page layout and remove unnecessary id parameter in UpdateBlogPage component

--- src/app/(authenticated)/admin/blogs/[id]/page.tsx
+++ src/app/(authenticated)/admin/blogs/[id]/page.tsx
@@ -4,12 +4,12 @@ import AdminLayout from '@/layouts/MainLayout/AdminLayout';
 import MainLayout from '@/layouts/MainLayout/MainLayout';
 import { useTheme } from '@/providers/context/Context';
 
-const UpdateBlogPage = ({ id }: { id: string }) => {
+const UpdateBlogPage = () => {
   const theme = useTheme();
   const { darkMode } = theme.state;
   return (
     <MainLayout>
-      <AdminLayout darkMode={darkMode}>Update Blog Page of {id}</AdminLayout>
+      <AdminLayout darkMode={darkMode}>Update Blog Page</AdminLayout>
     </MainLayout>
   );
 };
