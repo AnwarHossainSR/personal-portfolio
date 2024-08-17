303f7a54f4a77ec98454b4e9531763e81eb77add
Anwar Hossain <anwar.hossain@craftsmenltd.com>
2024-08-17 21:36:02 +0600

    added dynamic meta data and fav icon

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,8 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import type { Metadata, ResolvingMetadata } from 'next';
+
 import MainLayout from '@/layouts/MainLayout/MainLayout';
 
 import BlogDetailsPage from './BlogDetailsPage';
 
-const BkogDetails = ({ params }: { params: { slug: string } }) => {
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { slug } = params;
+  const post = await fetch(`${process.env.SITE_URL}/api/blogs/${slug}`).then(
+    res => res.json()
+  );
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: post.data.title,
+    openGraph: {
+      images: [post.data.image_url, ...previousImages],
+    },
+    description: post.data.short_content,
+  };
+}
+const BkogDetails = ({ params, searchParams }: Props) => {
   return (
     <MainLayout>
       <BlogDetailsPage slug={params.slug} />
